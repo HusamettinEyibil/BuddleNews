203e9b3e39c69c1331083937e1325c8e8656e8dc
Hüsamettin Eyibil <husamettin.eyibil@huawei.com>
2022-03-11 02:44:42 +0300

    Complete category cells structure.

--- BuddleNews/Controllers/SourcesViewController.swift
+++ BuddleNews/Controllers/SourcesViewController.swift
@@ -9,24 +9,103 @@ import UIKit
 
 class SourcesViewController: UIViewController {
     
+    private var collectionView: UICollectionView?
+    private var tableView = UITableView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        view.backgroundColor = .systemPink
-        fetchData()
+        title = "Sources"
+        view.backgroundColor = .systemBackground
+        configureCollectionView()
+        configureTableView()
+        getSources()
     }
     
-    private func fetchData() {
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        collectionView?.frame = CGRect(x: 0, y: view.safeAreaInsets.top, width: view.width, height: 35)
+        tableView.frame = CGRect(x: 0, y: view.safeAreaInsets.top + 50, width: view.width, height: 200)
+    }
+    
+    private func configureCollectionView() {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .horizontal
+        layout.estimatedItemSize = CGSize(width: 100, height: 35)
+        layout.sectionInset = UIEdgeInsets(top: 0, left: 10, bottom: 0, right: 10)
+        collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView?.showsHorizontalScrollIndicator = false
+        collectionView?.delegate = self
+        collectionView?.dataSource = self
+        collectionView?.backgroundColor = .systemBackground
+        collectionView?.register(CategoryCell.self, forCellWithReuseIdentifier: CategoryCell.identifier)
+        guard let collectionView = collectionView else {
+            return
+        }
+        view.addSubview(collectionView)
+    }
+    
+    private func configureTableView() {
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.showsVerticalScrollIndicator = false
+        tableView.backgroundColor = .systemPink
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        view.addSubview(tableView)
+    }
+    
+    private func getSources() {
         NetworkManager.shared.getSources { result in
             switch result {
             case .success(let response):
+                
                 break
             case .failure(let error):
                 print(error.localizedDescription)
             }
         }
     }
+}
 
+// MARK: - Collection View
+extension SourcesViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return Category.allCases.count
+    }
 
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: CategoryCell.identifier, for: indexPath) as? CategoryCell else {
+            return UICollectionViewCell()
+        }
+        cell.configure(with: Category.allCases[indexPath.row].rawValue)
+        return cell
+    }
+
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let text = Category.allCases[indexPath.row].rawValue
+        let width = self.estimatedFrame(text: text, font: UIFont.systemFont(ofSize: 14)).width
+        return CGSize(width: width+50, height: 35.0)
+    }
+
+    private func estimatedFrame(text: String, font: UIFont) -> CGRect {
+        let size = CGSize(width: 200, height: 1000) // temporary size
+        let options = NSStringDrawingOptions.usesFontLeading.union(.usesLineFragmentOrigin)
+        return NSString(string: text).boundingRect(with: size,
+                                                   options: options,
+                                                   attributes: [NSAttributedString.Key.font: font],
+                                                   context: nil)
+    }
+}
+
+extension SourcesViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.backgroundColor = .systemBlue
+        return cell
+    }
 }
 
---
+++ BuddleNews/Views/Cells/CategoryCell.swift
@@ -0,0 +1,75 @@
+//
+//  CategoryCell.swift
+//  BuddleNews
+//
+//  Created by Hüsamettin  Eyibil on 10.03.2022.
+//
+
+import UIKit
+
+class CategoryCell: UICollectionViewCell {
+    static let identifier = "CategoryCell"
+    
+    private let imageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(systemName: "plus")
+        imageView.tintColor = .label
+        imageView.contentMode = .scaleAspectFit
+        return imageView
+    }()
+    
+    private let categoryLabel: UILabel = {
+        let label = UILabel(frame: .zero)
+        label.textColor = .label
+        label.font = .systemFont(ofSize: 14, weight: .regular)
+        label.numberOfLines = 1
+        label.textAlignment = .center
+        label.sizeToFit()
+        return label
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        contentView.backgroundColor = .systemBackground
+        contentView.addSubview(imageView)
+        contentView.addSubview(categoryLabel)
+        contentView.clipsToBounds = true
+        contentView.layer.cornerRadius = contentView.height / 6
+        contentView.layer.borderColor = UIColor.label.cgColor
+        contentView.layer.borderWidth = 1
+        contentView.sizeToFit()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError()
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        imageView.frame = CGRect(x: 10, y: contentView.height/2 - 7,
+                                 width: 14,
+                                 height: 14)
+        categoryLabel.frame = CGRect(x: imageView.right+7, y: 5,
+                                     width: contentView.width-contentView.height-10,
+                                     height: contentView.height-10)
+    }
+    
+    override func preferredLayoutAttributesFitting(_ layoutAttributes: UICollectionViewLayoutAttributes) -> UICollectionViewLayoutAttributes {
+        setNeedsLayout()
+        layoutIfNeeded()
+        let size = contentView.systemLayoutSizeFitting(layoutAttributes.size)
+        var frame = layoutAttributes.frame
+        frame.size.width = ceil(size.width)
+        layoutAttributes.frame = frame
+        return layoutAttributes
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        categoryLabel.text = nil
+    }
+    
+    func configure(with labelText: String) {
+        categoryLabel.text = labelText
+    }
+}
